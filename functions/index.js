270c48bde52dab0ee17df0a855e9254274674615
Linh Vu <thuylinh2490@gmail.com>
2021-02-04 09:24:27 -0500

    database(firefunction): get articles

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,25 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 const db = admin.firestore();
-
+const app = express();
+app.use(cors({ origin: true }));
 
 //TEST POSTMAN
 exports.helloWorld = functions.https.onRequest((request, response) => {
      response.send("Hello from Firebase!");
 });
 
+app.get("/",async(req,res)=>{
+  const snapshot = await db.collection("articles").get()
+  let articles =[];
+  snapshot.forEach(doc=>{
+    let id= doc.id
+    let data = doc.data()
+    articles.push({id, ...data})
+    res.status(200).send(JSON.stringify(articles))
+  })
+})
 
-const app = express();
-app.use(cors({ origin: true }));
 app.post('/', async (req, res) => {
   const newArticle = req.body;
 
