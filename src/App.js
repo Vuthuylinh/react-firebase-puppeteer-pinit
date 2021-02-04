a21345f5f835928bffa4014421b6a815ef84d63c
Linh Vu <thuylinh2490@gmail.com>
2021-02-03 21:39:11 -0500

    feat(database): fetch article from Cloud firestore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React,{useState, useEffect}from "react";
+import firebase from "./firebaseConfig";
+
 import SignUp from "./components/auth/SignUp";
 
 const App =()=>{
-  return(
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const ref = firebase.firestore().collection("articles");
+  console.log("should be articles: ", ref);
 
-    <div>
-       <h1>Pinit Test</h1>
+  //ONE TIME GET FUNCTION
+  function getArticles() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const articles = [];
+      querySnapshot.forEach((doc) => {
+        articles.push(doc.data());
+      });
+      setArticles(articles);
+      setLoading(false);
+    });
+  }
+  useEffect(() => {
+    getArticles();
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <h1>Loading ...</h1>;
+  }
+  return (
+    <div className="App">
+      <div>
+        <h1>Articles</h1>
+        {articles.map((article) => (
+          <div key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+            <h4>{article.url}</h4>
+          </div>
+        ))}
+      </div>
+      {/* div to hook upthe signUp form on the button click*/}
+      <div>
         <SignUp />
       </div>
-  )
+    </div>
+  );
 }
 
 export default App
