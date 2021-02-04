07d8b8dd4a22a742f3e39fae43d2c7b891d74572
Linh Vu <thuylinh2490@gmail.com>
2021-02-04 09:05:14 -0500

    database(firebase function): post simple article successfully

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+
+//TEST POSTMAN
+exports.helloWorld = functions.https.onRequest((request, response) => {
+     response.send("Hello from Firebase!");
+});
+
+
+const app = express();
+app.use(cors({ origin: true }));
+app.post('/', async (req, res) => {
+  const newArticle = req.body;
+
+  await db.collection('articles').add( newArticle)
+  res.status(201).send();
+});
+
+
+ exports.api = functions.https.onRequest(app);
+
--- src/puppeteer/scrapeData.js
+++ src/puppeteer/scrapeData.js
@@ -1,22 +1,29 @@
 const puppeteer = require('puppeteer');
-const fse = require('fs-extra');
+
 const fs = require('fs')
 
-const url = 'https://en.wikipedia.org/wiki/Groundhog_Day';
-
-//GET TITLE and BODY of PAGE
+const url = 'https://dev.to/nickytonline/pairing-with-community-member-dan-ott-2ge';
 const newArticle = async()=> {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage()
   await page.goto(url, { waitUntil: 'networkidle2' });
 
+//GET TITLE and BODY of PAGE
   let title = await page.title();
-  // await page.waitForSelector('body')
-  // const body = await page.evaluate(() => document.body.innerHTML)
+  await page.waitForSelector('body')
+  const body = await page.evaluate(() => document.body.innerHTML)
 
+// console.log("TITLE: ",title)
+// console.log("BODY: ",body)
+
+//EXTRACT HTML FILE
   const html = await page.content();
   fs.writeFileSync("./outputHTML/index.html", html);
-  
+
+//
+   const htmlToString = JSON.stringify(html);
+
+  //  console.log("HTML TO STRING: ",htmlToString)
   await browser.close();
 }
 
